bd068924afdcdcd2fe0c78c756f7f7bcd54bc10d
Raniel Garcia <ranielgarcia2596@gmail.com>
2021-02-22 20:49:13 +0700

    Add session class for application state

---
+++ Main/Sessions.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Entities.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public static class Sessions
+    {
+        public static UserModel CurrentLoggedInUser { get; set; }
+
+
+    }
+}
