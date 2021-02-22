875384599db8356177c54cb7afe354847c3e6dd3
Raniel Garcia <ranielgarcia2596@gmail.com>
2021-02-22 21:20:17 +0700

    Added controller's response classes

---
+++ Main/Controllers/ListOfEntityResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Controllers
+{
+    public class ListOfEntityResult<Type> where Type : class
+    {
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public List<Type> Data { get; set; }
+    }
+}
