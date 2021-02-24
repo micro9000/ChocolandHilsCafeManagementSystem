759d9d190f08d736881c4091e78c0bee5a261ee8
Raniel Garcia <ranielgarcia2596@gmail.com>
2021-02-25 04:03:06 +0700

    Add data access methods

--- DataAccess/Data/EmployeeManagement/Contracts/IEmployeeAttendanceData.cs
+++ DataAccess/Data/EmployeeManagement/Contracts/IEmployeeAttendanceData.cs
@@ -10,5 +10,10 @@ namespace DataAccess.Data.EmployeeManagement.Contracts
 {
     public interface IEmployeeAttendanceData : IDataManagerCRUD<EmployeeAttendanceModel>
     {
+        List<EmployeeAttendanceModel> GetAllByEmployeeNumberAndWorkDateRange(string employeeNumber, DateTime startDate, DateTime endDate);
+        List<EmployeeAttendanceModel> GetAllByWorkDateRange(DateTime startDate, DateTime endDate);
+        List<EmployeeAttendanceModel> GetAllByShiftIdAndWorkDateRange(long shiftSchedId, DateTime startDate, DateTime endDate);
+        List<EmployeeAttendanceModel> GetAllLateEmployeesByWorkDateRange(DateTime startDate, DateTime endDate);
+        List<EmployeeAttendanceModel> GetAllEmployeesWithUndertimeByWorkDateRange(DateTime startDate, DateTime endDate);
     }
 }
--- DataAccess/Data/EmployeeManagement/Implementations/EmployeeAttendanceData.cs
+++ DataAccess/Data/EmployeeManagement/Implementations/EmployeeAttendanceData.cs
@@ -18,5 +18,68 @@ namespace DataAccess.Data.EmployeeManagement.Implementations
         {
             _dbConnFactory = dbConnFactory;
         }
+
+        public List<EmployeeAttendanceModel> GetAllByEmployeeNumberAndWorkDateRange(string employeeNumber, DateTime startDate, DateTime endDate)
+        {
+            string query = @"SELECT * FROM EmployeeAttendance
+                             WHERE isDeleted=false AND employeeNumber=@EmployeeNumber AND
+                                workDate BETWEEN @StartDate AND @EndDate";
+
+            return this.GetAll(query, new {
+                EmployeeNumber = employeeNumber,
+                StartDate = startDate,
+                EndDate = endDate
+            });
+        }
+
+        public List<EmployeeAttendanceModel> GetAllByShiftIdAndWorkDateRange(long shiftSchedId, DateTime startDate, DateTime endDate)
+        {
+            string query = @"SELECT * FROM EmployeeAttendance
+                             WHERE isDeleted=false AND shiftSchedId=@ShiftSchedId AND
+                                workDate BETWEEN @StartDate AND @EndDate";
+
+            return this.GetAll(query, new
+            {
+                ShiftSchedId = shiftSchedId,
+                StartDate = startDate,
+                EndDate = endDate
+            });
+        }
+
+        public List<EmployeeAttendanceModel> GetAllByWorkDateRange(DateTime startDate, DateTime endDate)
+        {
+            string query = @"SELECT * FROM EmployeeAttendance
+                             WHERE isDeleted=false AND workDate BETWEEN @StartDate AND @EndDate";
+
+            return this.GetAll(query, new
+            {
+                StartDate = startDate,
+                EndDate = endDate
+            });
+        }
+
+        public List<EmployeeAttendanceModel> GetAllEmployeesWithUndertimeByWorkDateRange(DateTime startDate, DateTime endDate)
+        {
+            string query = @"SELECT * FROM EmployeeAttendance
+                             WHERE isDeleted=false AND lateMins > 0 AND workDate BETWEEN @StartDate AND @EndDate";
+
+            return this.GetAll(query, new
+            {
+                StartDate = startDate,
+                EndDate = endDate
+            });
+        }
+
+        public List<EmployeeAttendanceModel> GetAllLateEmployeesByWorkDateRange(DateTime startDate, DateTime endDate)
+        {
+            string query = @"SELECT * FROM EmployeeAttendance
+                             WHERE isDeleted=false AND underTimeMins > 0 AND workDate BETWEEN @StartDate AND @EndDate";
+
+            return this.GetAll(query, new
+            {
+                StartDate = startDate,
+                EndDate = endDate
+            });
+        }
     }
 }
